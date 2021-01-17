627f89cb6ad0b31620e31a7f48044679b6c2cd2d
Manuel <manuelzlp@gmail.com>
2021-01-17 14:23:34 +0100

    first commit

---
+++ books.js
@@ -0,0 +1,14 @@
+const books = [
+	{title: 'I, Robot', author: 'Isaak Azimov', year: '1950'},
+	{title: 'The Martian Way and Other Stories', author: 'Isaak Azimov', year: '1955'},
+	{title: 'The Bicentennial Man and Other Stories', author: 'Isaak Azimov', year: '1976'},	
+	{title: 'Ten Little Niggers', author: 'Agatha Christie', year: '1944'},
+	{title: 'Lolita', author: 'Vladimir Nabokov', year: '1965'},
+	{title: 'The Call of Cthulhu', author: 'H. P. Lovecraft', year: '1928'},
+	{title: 'The Strange High House in the Mist', author: 'H. P. Lovecraft', year: '1931'},
+	{title: 'The Hobbit', author: 'J. R. R. Tolkien', year: '1937'},
+	{title: 'The Lord of the Rings', author: 'J. R. R. Tolkien', year: '1937'},
+	{title: 'Under the Dome', author: 'Stephen King', year: '2009'},
+	{title: 'The Shining', author: 'Stephen King', year: '1977'},
+	{title: 'It', author: 'Stephen King', year: '1986'},
+];
